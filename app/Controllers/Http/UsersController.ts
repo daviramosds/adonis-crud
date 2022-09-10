5f0083985fd55f8d621555edfa7d4d5d1767efd3
daviramosds <davirds.dev@gmail.com>
2022-09-10 07:20:29 -0300

    create user route

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,19 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async create() {
+  public async create({ request, response }: HttpContextContract) {
+    const { name, email, password } = request.only(['name', 'email', 'password'])
+
+    const newUser = await User.create({
+      name,
+      email,
+      password,
+    })
+
     return {
-      message: 'user created',
+      user: newUser,
     }
   }
 }
